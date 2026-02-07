2eac608578647ddd5ef8dcf71b4bb959c15c2ffd
elijahhollins <elijah.hollins@gmail.com>
2026-02-06 18:06:56 -0600

    Update initial transform scale and zoom limits in JobPrintMarkup.tsx

---
+++ JobPrintMarkup.tsx
@@ -0,0 +1,19 @@
+import React, { useRef, useEffect } from 'react';
+
+const JobPrintMarkup = () => {
+    const scale = 1; // Changed from 0.1 to 1
+
+    const handleWheel = (event) => {
+        const zoomScaleMin = 0.1;
+        const zoomScaleMax = 5;
+        // existing logic to handle zoom based on zoomScaleMin and zoomScaleMax
+    };
+
+    return (
+        <div>
+            {/* other component code */}
+        </div>
+    );
+};
+
+export default JobPrintMarkup;
